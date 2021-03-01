4f62e3476b15256c2b89eb289520a490105c3f5f
Bálint Szeleczky <szybalint@gmail.com>
2021-03-01 12:41:24 +0100

    Fix setterm on Windows

--- input_output/console_constants.py
+++ input_output/console_constants.py
@@ -1,3 +1,8 @@
+import os
+
 CLS = '\033[0;0f'  # clears the console window
-CURSOR_OFF = 'setterm -cursor off'  # removes the cursor from the console
+if os.name == 'posix':
+    CURSOR_OFF = 'setterm -cursor off'  # removes the cursor from the console
+else:
+    CURSOR_OFF = ''
 SLEEP_TIME = 0.01  # lower values == smoother animation
