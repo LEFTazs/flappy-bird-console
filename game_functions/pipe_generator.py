9ed0861bc21bbcfdaae0015993d4da32e431d44e
Bálint Szeleczky <szybalint@gmail.com>
2021-03-02 12:23:31 +0100

    Made pipes more Flappy Bird-like

--- game_functions/pipe_generator.py
+++ game_functions/pipe_generator.py
@@ -6,9 +6,9 @@ from game_objects.pipe import Pipe
 class PipeGenerator:
     @staticmethod
     def generate_pipe(width: int, height: int) -> Pipe:
-        max_pipe_hole_size = int(height / 2)
+        max_pipe_hole_size = int(height / 3)
         x = width
-        pipe_hole_size = random.choice(range(1, max_pipe_hole_size))
-        bottom_height = random.choice(range(1, height - max_pipe_hole_size))
+        pipe_hole_size = random.randint(4, max_pipe_hole_size)
+        bottom_height = random.randint(1, height - max_pipe_hole_size)
         top_height = bottom_height + pipe_hole_size
         return Pipe(x, top_height, bottom_height)
